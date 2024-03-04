1db7b6c993f59a5fcf8f7c1ebe66f615ffe4ca24
pradeep <kumarpradeepbgs@gmail.com>
2024-03-04 20:48:49 +0530

    added jwt and bcrypt

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import {client} from '../db/postgresDB.js'
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, registerUser } from "../controllers/user.controller.js";
 const router = Router();
 
 router.route('/').get((req,res)=>{
@@ -9,5 +9,5 @@ router.route('/').get((req,res)=>{
 })
 
 router.route('/register').post(registerUser)
-
+router.route('/login').post(loginUser)
 export default router;
